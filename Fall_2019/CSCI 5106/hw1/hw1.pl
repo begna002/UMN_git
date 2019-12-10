ff4408fb56daf7fd7eb5810dcc44fe2008804323
begna002 <begna002@umn.edu>
2019-12-10 12:49:00 -0600

    adding 5106 hw8

--- Fall_2019/CSCI 5106/hw1/hw1.pl
+++ Fall_2019/CSCI 5106/hw1/hw1.pl
@@ -1,9 +1,13 @@
-predicate(Value, true:[Value|Tail], false:Tail).
-predicate(_Value, false:Tail, true:Tail).
-
-oddOnly(List, LST) :-
- foldl(predicate, List, true:LST, _:[]).
+oddOnly([], LST) :- append([], [], LST).
+oddOnly([A], LST) :- append([A], [], LST).
+oddOnly([A,_| T], LST) :- oddOnly(T, TLST), append([A], TLST, LST).
 
 permutation(L, R) :- sort(L, X), sort(R, X).
 
 evenList(L) :- length(L, X), 0 is mod(X, 2).
+
+and(A,B):- A, B.
+or(A,B):- A; B.
+not(A):- \+ A.
+
+checker(X):- not(X).

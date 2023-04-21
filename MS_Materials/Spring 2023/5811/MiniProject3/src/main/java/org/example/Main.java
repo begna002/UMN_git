08f2fbb18acdb51ccc4a2d6eab7739b3d5713f7f
Moti Begna <motibegna@infra04-wg012.eduroam.wireless.umn.edu>
2023-04-21 10:50:11 -0500

    Adding 5811 intellij projects

--- MS_Materials/Spring 2023/5811/MiniProject3/src/main/java/org/example/Main.java
+++ MS_Materials/Spring 2023/5811/MiniProject3/src/main/java/org/example/Main.java
@@ -1,21 +1,21 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        compoundInterest();
-
-    }
-
-    public static void compoundInterest() {
-        double principal = 10000;
-        double rate = 10.25;
-        double time = 5;
-        time = time;
-
-        /* Calculate compound interest */
-        double CI = principal *
-                (Math.pow((1 + rate / 100), time));
-
-        System.out.println("Compound Interest is "+ CI);
-    }
+//    public static void main(String[] args) {
+//        compoundInterest();
+//
+//    }
+//
+//    public static void compoundInterest() {
+//        double principal = 10000;
+//        double rate = 10.25;
+//        double time = 5;
+//        time = time;
+//
+//        /* Calculate compound interest */
+//        double CI = principal *
+//                (Math.pow((1 + rate / 100), time));
+//
+//        System.out.println("Compound Interest is "+ CI);
+//    }
 }
